65394c24eafd0d611a858c62360d4b85ecf5fb1a
millenwanpsu <mwan@pdx.edu>
2018-11-20 15:49:01 -0800

    Reverted from validation split

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -17,31 +17,31 @@ def landpage():
     form_register = RegistrationForm()
     form_login = LoginForm()
     # Registration Form Submitted
-    if form_register.is_submitted():
-        if form_register.validate():
-            hashed_password = bcrypt.generate_password_hash(form_register.password.data).decode('utf-8')
-            db = db_model()
-            id = db.get_next_id()
-            db.add_user(id, form_register.username.data, form_register.email.data, 'default.jpg', hashed_password)
-            flash('Your account has been created! You are now able to log in', 'success')
-            return redirect(url_for('landpage'))
+    if form_register.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form_register.password.data).decode('utf-8')
+        db = db_model()
+        id = db.get_next_id()
+        db.add_user(id, form_register.username.data, form_register.email.data, 'default.jpg', hashed_password)
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('landpage'))
     # Login Form Submitted
-    elif form_login.is_submitted():
-        if form_login.validate():
-            db = db_model()
-            data = db.get_user_by_email(form_login.email.data)
-            if data and bcrypt.check_password_hash(data[0][4], form_login.password.data):
-                user = User(data[0][0], data[0][1], data[0][2], data[0][4])
-                login_user(user, remember=form_login.remember.data)
-                next_page = request.args.get('next')
-                return redirect(next_page) if next_page else redirect(url_for('dashboard'))
-            else:
-                flash('Login Unsuccessful. Please check email and password', 'danger')
+    if form_login.validate_on_submit():
+        db = db_model()
+        data = db.get_user_by_email(form_login.email.data)
+        if data and bcrypt.check_password_hash(data[0][4], form_login.password.data):
+            user = User(data[0][0], data[0][1], data[0][2], data[0][4])
+            login_user(user, remember=form_login.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('dashboard'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
     # Render both
     return render_template('landpage.html', title='Landing', form_l=form_login, form_r=form_register)
+
 @app.route("/dashboard")
 def dashboard():
     return render_template('dashboard.html', title='Dashboard')
+
 @app.route("/logout")
 def logout():
     logout_user()
