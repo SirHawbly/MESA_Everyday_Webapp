32143128a91d0a63ec8e11802cd393ac3287d6be
millenwanpsu <mwan@pdx.edu>
2018-11-29 13:50:00 -0800

    Added admin to __init__

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -5,6 +5,8 @@ https://github.com/CoreyMSchafer/code_snippets/blob/master/Python/Flask_Blog/06-
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flaskblog.views import MyModelView
+from flaskblog.models import User, Role, db_model
 
 
 app = Flask(__name__)
@@ -14,4 +16,11 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
+# Millen's admin creation
+# Modified from Flask-Admin example
+# https://github.com/flask-admin/flask-admin/blob/master/examples/auth/app.py
+admin = flask_admin.Admin(app,)
+# add model views
+admin.add_view(MyModelView(Role, db_model))
+admin.add_view(MyModelView(User, db_model))
 from flaskblog import routes
