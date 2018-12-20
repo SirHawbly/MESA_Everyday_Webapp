b95007c6b5957220b06d3dfe11d0e18c7e6f2e4c
Minwei <natsuyasum1@outlook.com>
2018-12-20 15:32:17 -0800

    support for resetting pwd & forgetting username

--- MESAeveryday/forms.py
+++ MESAeveryday/forms.py
@@ -4,7 +4,7 @@ https://github.com/CoreyMSchafer/code_snippets/blob/master/Python/Flask_Blog/06-
 """
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, DateField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, Regexp
 from MESAeveryday.models import User, School, loadSession, Badge, Stamp
 
 class RegistrationForm(FlaskForm):
@@ -21,14 +21,16 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
-        session = loadSession()
-        user = session.query(User).filter(User.username == username.data).first()
+        # session = loadSession()
+        # user = session.query(User).filter(User.username == username.data).first()
+        user = User.validate_username(username)
         if user:
             raise ValidationError('That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
-        session = loadSession()
-        user = session.query(User).filter(User.email == email.data).first()
+        # session = loadSession()
+        # user = session.query(User).filter(User.email == email.data).first()
+        user = User.validate_email(email)
         if user:
             raise ValidationError('That email is taken. Please choose a different one.')
 
@@ -46,17 +48,37 @@ class RequestResetForm(FlaskForm):
     submit = SubmitField('Request Password Reset')
 
     def validate_email(self, email):
-        session = loadSession()
-        user = session.query(User).filter(User.email == email.data).first()
-        if user is None:
+        # session = loadSession()
+        # user = session.query(User).filter(User.email == email.data).first()
+        user = User.validate_email(email)
+        if user == False:
             raise ValidationError('There is no account with that email. You must regiester first.')
 
 
+# class ResetPasswordForm(FlaskForm):
+#     password = PasswordField('Password', validators=[DataRequired()])
+#     confirm_password = PasswordField('Confirm Password',
+#                                      validators=[DataRequired(), EqualTo('password')])
+#     submit = SubmitField('Reset Password')
+
+class RequestResetUserForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Request User Reset')
+
+    def validate_email(self, email):
+        user = User.validate_email(email)
+        if user == False:
+            raise ValidationError('There is no account with that email. You must regiester first.')
+
 class ResetPasswordForm(FlaskForm):
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password',  validators=[DataRequired(), Length(min=8, message="your password must be at least %(min)d characters")
+                                 , Regexp("^(?=.*[0-9])(?=.*[!@#\$%\^&\*])",
+                                          message="The string must contain at least 1 numeric digit and 1 symbol")])
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Reset Password')
+
 
 class EarnStampsForm(FlaskForm):
     stamps = SelectField('Stamp', coerce=int)
@@ -67,4 +89,3 @@ class EarnStampsForm(FlaskForm):
         self.badge_name = badge_name
 
 
-        
