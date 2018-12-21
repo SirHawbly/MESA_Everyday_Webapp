383a2d322094cafb5e23fe66865cfb4076e2f7cd
Natsuyasum1 <natsuyasum1@outlook.com>
2018-12-20 19:33:41 -0800

    use single session instead of multiple ones

--- MESAeveryday/routes.py
+++ MESAeveryday/routes.py
@@ -5,7 +5,7 @@ https://github.com/CoreyMSchafer/code_snippets/blob/master/Python/Flask_Blog/06-
 from flask import render_template, url_for, flash, redirect, request
 from MESAeveryday import app, bcrypt, mail
 from MESAeveryday.forms import RegistrationForm, LoginForm, RequestResetForm, RequestResetUserForm, ResetPasswordForm, EarnStampsForm
-from MESAeveryday.models import User, Role, UserRole, School, Badge, Stamp, UserStamp, loadSession
+from MESAeveryday.models import User, Role, UserRole, School, Badge, Stamp, UserStamp, session  #, loadSession, loadLoginSession
 from flask_login import login_user, current_user, logout_user, login_required
 from flask_mail import Message
 from datetime import datetime
@@ -31,9 +31,10 @@ def register():
         hashed_password = bcrypt.generate_password_hash(form_register.password.data).decode('utf-8')
         new_user = User(form_register.username.data, form_register.firstname.data, form_register.lastname.data,
                         form_register.email.data, hashed_password, form_register.school.data)
-        with loadSession() as session:
-            session.add(new_user)
-            session.commit()
+        # with loadSession() as session:
+        #     session.add(new_user)
+        #     session.commit()
+        User.add_new_user(new_user)
         flash('Your account has been created! You are now able to log in', 'success')
         return redirect(url_for('landpage'))
     return render_template('landpage.html', title='Landing', form_l=form_login, form_r=form_register)
@@ -45,13 +46,12 @@ def login():
     form_register = RegistrationForm()
     form_login = LoginForm()
     if form_login.validate_on_submit():
-        with loadSession() as session:
-            user = session.query(User).filter(User.username == form_login.username.data).first()
-            user.last_login = datetime.now()
-            session.commit()
+        user = session.query(User).filter(User.username == form_login.username.data).first()
         if user and bcrypt.check_password_hash(user.password, form_login.password.data):
+            user.last_login = datetime.now()
             login_user(user, remember=form_login.remember.data)
             next_page = request.args.get('next')
+            session.commit()
             return redirect(next_page) if next_page else redirect(url_for('dashboard'))
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
@@ -89,8 +89,9 @@ def reset_request():
     form = RequestResetForm()
 
     if form.validate_on_submit():
-        with loadSession() as session:
-            user = session.query(User).filter(User.email == form.email.data).first()
+        user = User.get_user_by_email(form.email.data)
+        # with loadSession() as session:
+        #     user = session.query(User).filter(User.email == form.email.data).first()
         send_reset_email(user)
         flash('An email has been sent with instructions to reset your password.', 'info')
         return redirect(url_for('landpage'))
@@ -115,18 +116,19 @@ def reset_token(token):
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         print("Original: ", user.password)
         #updating the password requires first loading a new session 
-        with loadSession() as session:
-            #once a session is loaded we want to get the row 
-            #where User.id matches the id of the user returned by User.verify_reset_token(token)
-            #this insures that the password for the correct user will be the one changed
-            row = session.query(User).filter(User.id==user.id).first()
-            #Change the password is a simple assign statement
-            row.password = hashed_password
-            #Changes need to be committed in order to make it to the database
-            session.commit()
-        #send a message to the user telling them that there account has been updated successfully
-        flash('Your password has been updated! You are now able to log in', 'success')
-        return redirect(url_for('landpage'))
+        # with loadSession() as session:
+        #     #once a session is loaded we want to get the row 
+        #     #where User.id matches the id of the user returned by User.verify_reset_token(token)
+        #     #this insures that the password for the correct user will be the one changed
+        #     row = session.query(User).filter(User.id==user.id).first()
+        #     #Change the password is a simple assign statement
+        #     row.password = hashed_password
+        #     #Changes need to be committed in order to make it to the database
+        #     session.commit()
+        # #send a message to the user telling them that there account has been updated successfully
+        if User.reset_pwd(user.id, hashed_password) == True:
+            flash('Your password has been updated! You are now able to log in', 'success')
+            return redirect(url_for('landpage'))
     return render_template('reset_token.html', title='Rest Password', form=form)
 
 @app.route("/reset_user", methods=['GET', 'POST'])
@@ -136,9 +138,11 @@ def reset_user():
 
     form = RequestResetUserForm()
     if form.validate_on_submit():
-        with loadSession() as session:
-            user = session.query(User).filter(User.email == form.email.data).first()
-            send_reset_user(user)
+        # with loadSession() as session:
+        #     user = session.query(User).filter(User.email == form.email.data).first()
+        #     send_reset_user(user)
+        user = User.get_user_by_email(form.email.data)
+        send_reset_user(user)
         flash('An email has been sent with your username.', 'info')
         return redirect(url_for('landpage'))
     return render_template('reset_user.html', title='Rest User', form=form)
@@ -158,7 +162,8 @@ def send_reset_user(user):
 def earn_stamps():
     result = [row.badge_name for row in Badge.get_all_badges_names()]                                   # maintain some objects in dashboard
     badges = {row.badge_id : row.badge_name for row in Badge.get_all_badges_id_with_names()}.items()    # a dictionary -> badge_id : badge_name
-    id = current_user.id    # acquire the user_id of current user
+    # with loadSession() as session:
+        # id = current_user.id    # acquire the user_id of current user
     forms, t = [], 1        # several pairs of forms (stamp, date, submit). t is used to assign unique id
     for badge in badges:
         # unearned = Stamp.get_unearned_stamps_of_badge(id, badge[0])  # get unearned stamps of current user
@@ -177,13 +182,14 @@ def earn_stamps():
         if form.submit.data:
             if form.validate_on_submit():
                 print('is submitted')
+                id = current_user.id    # acquire the user_id of current user
+                print(id)
                 if UserStamp.earn_stamp(id, form.stamps.data, datetime.now(), form.time_finished.data.strftime('%Y-%m-%d')) == True:
                     # flash('You\'ve earned a new stamp!', 'success')
                     # some message should be shown here, flash doesnt work
                     print('k.')
-                else:
-                    print("nah")
-                    # flash('Stamp already exists!', 'warning')
+                # else:
+                    # flash('Failed adding stamp', 'warning')
                     # some message should be shown here, flash doesnt work
                 return redirect('/dashboard')   # could be redirected to either dashboard or the same page?
     return render_template('earnstamps.html', title='Earn Stamps', forms=forms, result=result)
