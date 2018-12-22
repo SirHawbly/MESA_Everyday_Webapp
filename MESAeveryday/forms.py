4c9af33722a450669f6e569dea9426321410d00e
minh9 <minh9@pdx.edu>
2018-12-22 12:20:34 -0800

    update forms.py

--- MESAeveryday/forms.py
+++ MESAeveryday/forms.py
@@ -35,6 +35,7 @@ class RegistrationForm(FlaskForm):
             raise ValidationError('That email is taken. Please choose a different one.')
 
 
+
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     password = PasswordField('Password', validators=[DataRequired()])
