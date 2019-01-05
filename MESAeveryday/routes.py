ced451b7080bb9f89737f43ab3780920b73c4c1a
minh9 <minh9@pdx.edu>
2019-01-04 22:11:18 -0800

    update route for account page

--- MESAeveryday/routes.py
+++ MESAeveryday/routes.py
@@ -33,6 +33,7 @@ def register():
         hashed_password = bcrypt.generate_password_hash(form_register.password.data).decode('utf-8')
         new_user = User(form_register.username.data, form_register.firstname.data, form_register.lastname.data,
                         form_register.email.data, hashed_password, form_register.school.data)
+
         session = loadSession()
         session.add(new_user)
         session.commit()
@@ -259,25 +260,42 @@ login to the app. '''
     mail.send(msg)
 
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/img', picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
 
 @app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
     form = UpdateAccountForm()
+
     if form.validate_on_submit():
-     #   if form.picture.data:
-     #       picture_file = save_picture(form.picture.data)
-     #       current_user.image_file = picture_file
-        current_user.username = form.username.data
-        current_user.email = form.email.data
-        #db.session.commit()
+        session = loadSession()
+        myaccount = session.query(User).filter(User.username == current_user.username).first()
+
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            myaccount.picture=picture_file
+            myaccount.school = form.school.data
+
+        session.commit()
         flash('Your account has been updated!', 'success')
         return redirect(url_for('account'))
     elif request.method == 'GET':
 
         form.username.data = current_user.username
         form.email.data = current_user.email
-    image_file = url_for('static', filename='profile_pics/' ) #+ current_user.image_file)
+        print(current_user.picture)
+    image_file = url_for('static', filename='img/' + current_user.picture)
     return render_template('account.html', title='Account',
                            image_file=image_file, form=form)
 
@@ -286,7 +304,7 @@ def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    picture_path = os.path.join(app.root_path, 'static/img', picture_fn)
 
     output_size = (125, 125)
     i = Image.open(form_picture)
