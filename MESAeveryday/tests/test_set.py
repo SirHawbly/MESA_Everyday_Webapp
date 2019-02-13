a017ddaafafd3eb252019cc7dbe0151de5739702
SirHawbly <silversteinrulz@gmail.com>
2019-02-12 19:06:00 -0800

    moving tests to a different branch

--- MESAeveryday/tests/test_set.py
+++
@@ -1,6 +0,0 @@
-import base64
-
-def test_tc1():
-    given="49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-    expected=b"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
-    assert base64.b64encode(bytes.fromhex(given)) == expected
