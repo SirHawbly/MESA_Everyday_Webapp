6458f09dd103c7a8ee31ccaf3a8466f5cf9af885
Minwei <natsuyasum1@outlook.com>
2019-02-18 10:56:59 -0800

    disabled CSRF

--- MESAeveryday/__init__.py
+++ MESAeveryday/__init__.py
@@ -12,6 +12,7 @@ from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['WTF_CSRF_ENABLED'] = False
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
