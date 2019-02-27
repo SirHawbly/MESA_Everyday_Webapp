387cd1b5d1fc14ea73ceb90213d6e013187af939
mcohoe <mcohoe@pdx.edu>
2019-02-27 15:24:06 -0800

    Made years inactive start at 10 instead of 1

--- MESAeveryday/forms.py
+++ MESAeveryday/forms.py
@@ -144,7 +144,7 @@ class DeleteStampForm(FlaskForm):
     submit = SubmitField('Delete Stamp')
 
 class RemoveOldAccountsForm(FlaskForm):
-    years = SelectField('Years Inactive:', choices=[(1,'1'),(2,'2'),(3,'3'),(4,'4'),(5,'5'),(6,'6'),(7,'7'),(8,'8'),(9,'9'),(10,'10')], coerce=int)
+    years = SelectField('Years Inactive:', choices=[(10,'10'),(9,'9'),(8,'8'),(7,'7'),(6,'6'),(5,'5'),(4,'4'),(3,'3'),(2,'2'),(1,'1')], coerce=int)
     submit = SubmitField('Delete Old Accounts')
 
 class ResetDateForm(FlaskForm):
